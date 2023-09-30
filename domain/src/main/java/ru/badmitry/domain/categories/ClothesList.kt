11bd15f0319b6d4f890e6a15cef4813060c2df50
grishindk <grishindk@msk.bcs.ru>
2023-09-30 18:27:58 +0300

    add some cards

--- domain/src/main/java/ru/badmitry/domain/categories/ClothesList.kt
+++ domain/src/main/java/ru/badmitry/domain/categories/ClothesList.kt
@@ -35,10 +35,5 @@ object ClothesList {
             imageResId = R.drawable.clothes_shirt,
             nameSoundResId = R.raw.clothes_shirt,
         ),
-        Entity(
-            nameResId = R.string.clothes_pants,
-            imageResId = R.drawable.clothes_pants,
-            nameSoundResId = R.raw.clothes_pants,
-        ),
     )
 }
